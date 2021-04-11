16f4324b703f852cb0449ba0134dccd6a18781ac
jung su young <j9972@naver.com>
2021-04-12 01:07:55 +0900

    change style

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './App.css';
-//import Axios from 'axios';
+import Axios from 'axios';
 
-const App = () => { };
+const App = () => { 
+    return (
+        <div className="App">
+            <div className="registration">
+                <h1>Registration</h1>
+                <label>Username</label>
+                <input type="text" />
+                <label>Password</label>
+                <input type="text" />
+                <button> Register </button>
+            </div>
+            <div className="login">
+                <h1>Registration</h1>
+                <input type="text" placeholder="Username..." />
+                <input type="password" placeholder="Password..." />
+                <button> Register </button>
+            </div>
+        </div>
+    )
+};
 
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
-const bodyParser = require("body-parser");
 const express = require("express");
-const cors = require('cors');
-const app = express();
 const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "password",
+    database: "LogionSystem",
+})
 
 app.listen(3001, () => {
     console.log("running on 3001");
