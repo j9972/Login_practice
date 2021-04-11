ec387c30615b689c67075de261f90be0bf1b5582
jung su young <j9972@naver.com>
2021-04-12 01:25:20 +0900

    change input's property

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Axios from 'axios';
 
 const App = () => { 
+
+    const [usernameReg, setUsernameReg] = useState('')
+    const [passwordReg, setPasswordReg] = useState('')
+
     return (
         <div className="App">
             <div className="registration">
                 <h1>Registration</h1>
                 <label>Username</label>
-                <input type="text" />
+                <input 
+                    type="text" 
+                    onChange={(e)=>{
+                        setUsernameReg(e.target.value);
+                    }}
+                />
                 <label>Password</label>
-                <input type="text" />
+                <input 
+                    type="text" 
+                    onChange={(e)=>{
+                        setPasswordReg(e.target.value);
+                    }}
+                />
                 <button> Register </button>
             </div>
             <div className="login">
-                <h1>Registration</h1>
+                <h1>Login</h1>
                 <input type="text" placeholder="Username..." />
                 <input type="password" placeholder="Password..." />
                 <button> Register </button>
